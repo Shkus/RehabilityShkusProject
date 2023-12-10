6f8d3e1c1f269f3ab8fff8c1900fc4dc3361ffcb
Shkus <virtualshkus@yandex.ru>
2023-12-10 23:31:55 +0500

    addSecondtestCalss

---
+++ RehabilityApplication.CoreLib/testSecondAfterRemoteShkusRehability.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RehabilityApplication.CoreLib
+{
+	public class testSecondAfterRemoteShkusRehability
+	{
+		//delete this class
+
+	}
+}
